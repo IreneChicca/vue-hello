30c852d83515984d825f4189517299a5ac391ad9
IreneChicca <irene.chicca@outlook.com>
2023-08-29 13:32:31 +0200

    ADD class txt

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ createApp({
   data() {
     return {
       message: 'Hello World! (:',
-      imgpath: './img/paw.png'
+      imgpath: './img/paw.png',
+      textCenter: 'text-center'
     }
   }
 }).mount('#app')
