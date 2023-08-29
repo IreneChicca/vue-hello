d615a550d1b08011163666d35b9da9c3515c2701
IreneChicca <irene.chicca@outlook.com>
2023-08-29 13:12:21 +0200

    ADD vue

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert('prova')
+const { createApp, ref } = Vue
+
+  createApp({
+    setup() {
+      const message = ref('Hello vue!')
+      return {
+        message
+      }
+    }
+  }).mount('#app')
